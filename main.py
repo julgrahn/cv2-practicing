3f01127ee235205194039adf694f5aa8d8b7a224
julgrahn <alexander.grahn@hotmail.se>
2020-07-06 23:52:59 +0200

    create new image with new res

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ print(img.ndim)
 
 resizedImage = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
 cv2.imshow("Galaxy", resizedImage)
+cv2.imwrite("GalaxyResized.jpg", resizedImage)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
